eb31420e0b82c7d4aa48e28fba61c4be822a6cf5
IgnacioNGarcia <117124643+IgnacioNGarcia@users.noreply.github.com>
2023-10-03 22:11:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ let tiempo = 0;
 let vueltas = 0;
 let tiempo_pomodoro = 25,tiempo_descanso_simple = 5, tiempo_descanso_largo = 15;
 const mood = document.getElementById("mood");
+const barra_progreso = document.getElementById("progreso");
 
 function iniciar_timer(){
     const lugar_timer = document.getElementById("timer")
     const lugar_modo = document.getElementById("modo")
+    if(vueltas <3){
+        barra_progreso.classList.remove("bg-info");
+        mood.classList.remove("mood-descanso-simple");
+
+    }else{
+        barra_progreso.classList.remove("bg-success");
+        mood.classList.remove("mood-descanso-largo");
+
+    }
+    barra_progreso.classList.add("bg-danger");
+    mood.classList.add("mood-pomo");
     lugar_modo.innerHTML = 'Modo concentración'
     lugar_timer.innerHTML = '';
     let minutos=0, segundos = 0;
@@ -14,7 +26,8 @@ function iniciar_timer(){
     intervalo = setInterval(function(){
         minutos = Math.floor(tiempo/ 60); 
         segundos = tiempo % 60; 
-
+        actualizar_progreso(minutos, segundos, tiempo_pomodoro)
+        
         if(minutos <= tiempo_pomodoro){
             lugar_timer.innerHTML = `${minutos}:${segundos}`
             tiempo++;
@@ -36,8 +49,12 @@ function iniciar_timer(){
 
 
 function descanso_simple(){
+    barra_progreso.classList.remove("bg-danger");
+    barra_progreso.classList.add("bg-info");
+
     mood.classList.remove("mood-pomo");
     mood.classList.add("mood-descanso-simple");
+
     const lugar_timer = document.getElementById("timer")
     lugar_timer.innerHTML = '';
     const lugar_modo = document.getElementById("modo")
@@ -46,7 +63,7 @@ function descanso_simple(){
     intervalo = setInterval(function(){
         minutos = Math.floor(tiempo/60);
         segundos = tiempo % 60;
-
+        actualizar_progreso(minutos, segundos, tiempo_descanso_simple)
         if(minutos <= tiempo_descanso_simple){
             lugar_timer.innerHTML = `${minutos}:${segundos}`;
             tiempo++;
@@ -64,14 +81,20 @@ function descanso_simple(){
 function descanso_largo(){
     mood.classList.remove("mood-pomo");
     mood.classList.add("mood-descanso-largo");
+
+    barra_progreso.classList.remove("bg-danger");
+    barra_progreso.classList.add("bg-success");
+
     const lugar_timer = document.getElementById("timer")
     lugar_timer.innerHTML = '';
     const lugar_modo = document.getElementById("modo")
     lugar_modo.innerHTML = 'Modo chill 15 min'
 
     intervalo = setInterval(function(){
+        vueltas = 0;
         minutos = Math.floor(tiempo/60);
         segundos = tiempo % 60;
+        actualizar_progreso(minutos, segundos, tiempo_descanso_largo)
 
         if(minutos <= tiempo_descanso_largo){
             lugar_timer.innerHTML = `${minutos}:${segundos}`;
@@ -95,4 +118,11 @@ function detener_timer(){
 function ruido_timer_finalizado(){
     const audio = new Audio("microwave-timer-117077.mp3");
     audio.play();
+}
+
+function actualizar_progreso(minutos, segundos, tiempo_maximo){
+    let porcentaje = (((minutos *60) + segundos)/ (tiempo_maximo * 60)) * 100;
+    
+    barra_progreso.style.width = porcentaje + "%";
+
 }
